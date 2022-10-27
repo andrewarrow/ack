99dc6cc8b05bc1c50447ec8e86d38a9b946cde6d
Andrew Arrow <oneone@gmail.com>
2022-10-27 15:54:25 -0700

    adding server and client

--- tcp/handshake.go
+++ tcp/handshake.go
@@ -1,13 +1,37 @@
 package tcp
 
 type Server struct {
+	State string
+	Port  int
+}
+
+type Client struct {
+	State string
 }
 
 func NewServer() *Server {
 	s := Server{}
+	s.State = "CLOSED"
 	return &s
 }
 
-func (s *Server) Listen(port int) {
+func NewClient() *Client {
+	c := Client{}
+	c.State = "CLOSED"
+	return &c
+}
 
+func (s *Server) Listen(port int) {
+	s.State = "LISTEN"
+	s.Port = port
+	go s.ListenForMessages()
+}
+
+func (c *Client) Connect(port int) {
+	c.State = "SYN-SENT"
+}
+
+func (s *Server) ListenForMessages() {
+	for {
+	}
 }
--- tcp/handshake_test.go
+++ tcp/handshake_test.go
@@ -5,4 +5,12 @@ import "testing"
 func TestHandshake(t *testing.T) {
 	s := NewServer()
 	s.Listen(80)
+	if s.State != "LISTEN" {
+		t.Fatal("wrong state", s.State)
+	}
+	c := NewClient()
+	c.Connect(80)
+	if c.State != "SYN-SENT" {
+		t.Fatal("wrong state", c.State)
+	}
 }
